315883bb33788bd87ee6da693c1882fcb65237a2
Atul Sharma <atulanilsharma@gmail.com>
2022-04-25 21:23:38 +0530

    Go-Results MVP

---
+++ CHANGELOG.md
@@ -0,0 +1,19 @@
+All notable changes to this project will be documented in this file.
+We follow the [Semantic Versioning 2.0.0](http://semver.org/) format.
+
+
+## 0.1.0 - 2022-04-25
+
+### Added
+- A "Result" interface and factory method for creation
+- Decorator function to wrap exisint function calls in "Result" objects
+- Unit tests for the features listed above
+
+### Deprecated
+- Nothing.
+
+### Removed
+- Nothing.
+
+### Fixed
+- Nothing.
---
+++ results_test.go
@@ -0,0 +1,58 @@
+package gresults
+
+import (
+	"errors"
+	"strconv"
+	"testing"
+)
+
+func TestGenericsErrorHandler(t *testing.T) {
+	want := 1
+	errStr := "this failed!"
+	strErrorResult := NewResult(&want, &errStr)
+	errorHandler := func(err string) {
+		if err != errStr {
+			t.Errorf("got error %q, wanted %q", err, errStr)
+		}
+	}
+	_ = strErrorResult.OnError(errorHandler).Unwrap(0)
+}
+
+func TestPanicIfErrHandlerAbsent(t *testing.T) {
+	err := errors.New("this failed!")
+	defer func() {
+		if r := recover(); r != nil {
+			if !errors.Is(r.(error), err) {
+				t.Errorf("expected panic with error %q, wanted %q", r, err)
+			}
+		} else {
+			t.Errorf("got nil, expected unwrap to panic")
+		}
+	}()
+	panicingResult := NewResult[int](nil, &err)
+	_ = panicingResult.Unwrap(0)
+}
+
+func TestSuccessfulUnwrap(t *testing.T) {
+	want := 1
+	successResult := NewResult[int, error](&want, nil)
+	errorHandler := func(err error) {
+		t.Errorf("got err %q, wanted nil", err)
+	}
+	got := successResult.OnError(errorHandler).Unwrap(0)
+	if got != want {
+		t.Errorf("unwrap failed. got %q, wanted %q", got, want)
+	}
+}
+
+func TestWrappedCallable(t *testing.T) {
+	want := -42
+	wrappedAtoiResult := Resultify[int, error](strconv.Atoi, "-42")
+	errorHandler := func(err error) {
+		t.Errorf("got err %q, wanted nil", err)
+	}
+	got := wrappedAtoiResult.OnError(errorHandler).Unwrap(0)
+	if got != want {
+		t.Errorf("unwrap failed. got %q, wanted %q", got, want)
+	}
+}
